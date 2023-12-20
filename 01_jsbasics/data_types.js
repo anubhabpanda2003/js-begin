96e5d9435c9cd996937c53a831a6e94a8cc66fed
Anubhab Panda <120190340+anubhabpanda2003@users.noreply.github.com>
2023-12-20 14:31:49 +0000

    datatypes_learned

---
+++ 01_jsbasics/data_types.js
@@ -0,0 +1,18 @@
+// console.log("working");
+// const id = Symbol('123')
+// const anotherid = Symbol('123')
+// console.log(id===anotherid)
+// const arr = ["anubhab" , "panda" , "omm"];
+// let obj = {
+//     name: "Anubhab Panda ",
+//     regno: 2241016054 ,
+//     email: "anubhabpanda990@gmail.com" ,
+//     password: "password"
+
+// }
+// console.log(arr);
+// console.log(obj);
+const myfunction = function(){
+    console.log("Anubhab Panda from myfunction");
+}
+console.log(typeof myfunction());
