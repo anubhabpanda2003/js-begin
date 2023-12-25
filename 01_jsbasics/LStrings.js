b72a9d6c638048f149ee25bd374be06c8d5da179
Anubhab Panda <120190340+anubhabpanda2003@users.noreply.github.com>
2023-12-25 14:40:44 +0000

    String_learn

---
+++ 01_jsbasics/LStrings.js
@@ -0,0 +1,24 @@
+// const name = "Anubhb panda"
+// const age = 19
+// console.log("my name is:" +name +"my age is "+age)
+// console.log(`Hello my name is ${name} , and my age is ${age}`)// backtext to be followed
+const name = "Anubhab";
+const gameName = new String('Anubhab-panda-is-a-good-buy');
+// console.log(typeof(gameName));
+// console.log(typeof(name));
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+// console.log(gameName.charAt(2));
+// console.log(gameName[2]);
+// console.log(gameName.indexOf('b'));
+// const newName = gameName.substring(1,4);
+// console.log(newName);
+// const sName = gameName.slice(-7 , 4);
+// console.log(sName);
+// const newString = "  K Ghost   ";
+// console.log(newString);
+// console.log(newString.trim());
+// const url = "https://www.yootube.com/%20home";
+// console.log(url.replace('%20', '_' ));
+// console.log(url.includes('yoo'));
+console.log(gameName.split('-'));
