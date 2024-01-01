9f5c3f8c6e284180166f8c2c988524907a5c0255
Anubhab Panda <120190340+anubhabpanda2003@users.noreply.github.com>
2024-01-01 13:48:22 +0000

    modifyNumbers

--- 01_jsbasics/numbers.js
+++ 01_jsbasics/numbers.js
@@ -1,6 +1,23 @@
 const score = 400;
 
-const balance = new Number(100);
-console.log(balance);
-console.log(balance.toString);
-console.log(typeof(balance));
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed());
+
+// const anumber = 123.392;
+// console.log(anumber.toPrecision(4));
+
+// const num = 1000000;
+// console.log(num.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++  MATHS  ++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.floor(1.5));
+console.log(Math.min(4,5,6,2,8,2,9));
+
+
+
+
